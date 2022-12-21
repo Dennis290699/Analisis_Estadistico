714948ae600df7e59a6b2476a157b23249bbb715
Dennis290699 <88905858+Dennis290699@users.noreply.github.com>
2022-12-20 21:29:05 -0500

    Update Script_Total_Presos.R

--- Scripts/Script_Total_Presos.R
+++ Scripts/Script_Total_Presos.R
@@ -12,24 +12,24 @@ library (readxl)
 library(aplpack)
 #Esta siguiente linea de codigo, nos sirve para enlazar,
 #el excel a R
-respositorio_datos <- "C:\\Users\\chimuelo\\Desktop\\deber_proba"
+respositorio_datos <- "RUTA DEL ARCHIVO GUARDADA EN SU PC .XLSX"
 setwd(respositorio_datos)
 Informacion <- read_xlsx("TotalPresos.xlsx",sheet= "Presos",range = "B1:B221")
 
 #Este comando "summary"
 #este comando muestra un resumen general sobre las variables del data frame 
-#(mínimo, máximo, media, mediana, primer y tercer cuartil). 
+#(mÃ­nimo, mÃ¡ximo, media, mediana, primer y tercer cuartil). 
 summary(Informacion)
 #Diagrama de cajas
 boxplot(Informacion,main="boxplot-Presos Total", col = "blue", horizontal=TRUE )
-#(el método jitter evitará dibujar sobre los datos atípicos)
+#(el mÃ©todo jitter evitarÃ¡ dibujar sobre los datos atÃ­picos)
 stripchart(Informacion,main="boxplot-Presos Total",method = "jitter", add = TRUE,col = "blue", horizontal=TRUE )
 #Histograma
 hist(x = Informacion$Presos_tot, xlab="Presos", ylab="Frecuencias",
 main="Total de Presos", col = "red")
 #Tallo y hoja
-#La función stem.leaf del paquete aplpack es una alternativa 
-#a la función stem de base R, que permite configuraciones adicionales. 
+#La funciÃ³n stem.leaf del paquete aplpack es una alternativa 
+#a la funciÃ³n stem de base R, que permite configuraciones adicionales. 
 stem.leaf(Informacion$Presos_tot, m=4)
 
 
