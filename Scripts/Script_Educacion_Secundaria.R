f5b14f8c145ab41b1d60378276ec3ea69593eab1
Dennis290699 <88905858+Dennis290699@users.noreply.github.com>
2022-12-20 21:27:54 -0500

    Update Script_Educacion_Secundaria.R

--- Scripts/Script_Educacion_Secundaria.R
+++ Scripts/Script_Educacion_Secundaria.R
@@ -1,5 +1,5 @@
 #Tenemos el Segundo indicador
-#Este indicador es "Educación Secundaria "
+#Este indicador es "EducaciÃ³n Secundaria "
 
 #En esta parte estamos instalando el paquete
 install.packages("ggplot2")
@@ -12,22 +12,22 @@ library (readxl)
 library(aplpack)
 #Esta siguiente linea de codigo, nos sirve para enlazar,
 #el excel a R
-respositorio_datos <- "C:\\Users\\chimuelo\\Desktop\\deber_proba"
+respositorio_datos <- "RUTA DEL ARCHIVO EN SU PC .XLSX"
 setwd(respositorio_datos)
 Informacion1 <- read_xlsx("Secundaria.xlsx",sheet="Sheet1",range = "B1:B221")
 
 #Este comando "summary"
 #este comando muestra un resumen general sobre las variables del data frame 
-#(mínimo, máximo, media, mediana, primer y tercer cuartil). 
+#(mÃ­nimo, mÃ¡ximo, media, mediana, primer y tercer cuartil). 
 summary(Informacion1)
 #Diagrama de cajas
 boxplot(Informacion1,main="boxplot, Estudiantes de Secundaria", col = "red", horizontal=TRUE )
-#(el método jitter evitará dibujar sobre los datos atípicos)
+#(el mÃ©todo jitter evitarÃ¡ dibujar sobre los datos atÃ­picos)
 stripchart(Informacion1,main="boxplot, Estudiantes de Secundaria",method = "jitter", add = TRUE,col = "red", horizontal=TRUE )
 #Histograma
 hist(x = Informacion1$secundaria_total, xlab="Num_Estudiantes", ylab="Frecuencias",
      main="Total de Estudiantes", col = "blue")
 #Tallo y hoja
-#La función stem.leaf del paquete aplpack es una alternativa 
-#a la función stem de base R, que permite configuraciones adicionales. 
+#La funciÃ³n stem.leaf del paquete aplpack es una alternativa 
+#a la funciÃ³n stem de base R, que permite configuraciones adicionales. 
 stem.leaf(Informacion1$secundaria_total, m=4)
